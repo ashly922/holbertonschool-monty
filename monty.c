b4199fd1d1b71a536da67625e6b1251f9efe4557
ashly922 <ashly922@yahoo.com>
2023-04-03 15:13:01 -0700

    corrected global var

--- monty.c
+++ monty.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-char *value = NULL;
-
 /**
  * free_list - free the list
  * @head: pointer to a stack_t
@@ -17,6 +15,7 @@ void free_list(stack_t *head)
 		head = tmp;
 	}
 }
+
 /**
  * main - main function
  * @argc: int
@@ -30,35 +29,15 @@ int main(int argc, char *argv[])
 	int line_number = 0;
 	void (*opcode_func)(stack_t **stack, unsigned int line_number);
 	stack_t *head = NULL;
+	char *value = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-<<<<<<< HEAD
 	fp = fopen(argv[1], "r");
 	if (!fp)
-=======
-	free(lines);
-}
-
-/**
- *
- *
- *
- *
- */
-void execute_lines(char** lines, int num_lines)
-{
-	int i= 0;
-	char* arg;
-	int data = atoi(arg);
-	int lines_num = i + 1;
-	char* token = strtok(lines, " ");
-
-	for (i = 0; i < num_lines; i++)
->>>>>>> 707df782b7b892e9626ffbfb0dde0cc145698217
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
@@ -69,41 +48,13 @@ void execute_lines(char** lines, int num_lines)
 		token = strtok(buffer, "\n\t $");
 		if (!token)
 			continue;
-<<<<<<< HEAD
+		value = strtok(NULL, "\n\t $");
 		opcode_func = op_func(token);
 		if (!opcode_func)
-=======
-		}
-		if (strcmp(opcode, "push") == 0)
-		{
-			char* arg = strtok(NULL, "\n");
-
-			if (arg == NULL)
-			{
-				fprintf(stderr, "L%d: usage: push integer\n", lines_num);
-				exit(EXIT_FAILURE);
-			}
-
-			if (data == 0 && arg[0] != '0')
-			{
-				fprintf(stderr, "L%d: usage: push integer\n", lines_num);
-				exit(EXIT_FAILURE);
-			}
-
-			int top = 0;
-			int line_number = 0;
-			push(&top, data, line_number);
-		}
-
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(top);
-		}
-		else
->>>>>>> 707df782b7b892e9626ffbfb0dde0cc145698217
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
 			fclose(fp);
+			free_list(head);
 			exit(EXIT_FAILURE);
 		}
 		opcode_func(&head, line_number);
